0d52592e43f01498227b70254047a7d9e50aa7d7
George515600 <bejangeorge@gmail.com>
2013-07-11 10:33:07 +0000

    rest of the files added

---
+++ web.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+var file = fs.readFileSync("index.html", "utf8")
+app.get('/', function(request, response) {
+  response.send(file);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
